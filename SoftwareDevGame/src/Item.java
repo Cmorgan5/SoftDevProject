0942d8db46dac8ab9f4a5f4b1cf8282f35d2390b
Cmorgan5 <Cory_Morgan16@yahoo.com>
2015-04-01 13:36:13 -0400

    Item Code

---
+++ SoftwareDevGame/src/Item.java
@@ -0,0 +1,83 @@
+/**
+ * Class: Item
+ * @author: Cory morgan
+ * @version 1.0 
+ * Course: ITEC 
+ */
+
+
+public class Item {
+	
+	
+	
+	
+	private String name;
+	private int value;
+	private boolean weapon;
+
+	/** Method Item
+	 * Constructor
+	 * 
+	 * @param name - String representing the item name
+	 */
+	public Item(String name) {
+		this.name = name;
+		weapon = false;
+		this.value = value;
+	}
+
+	/** Method getName
+	 * gets the name of the item
+	 * 
+	 * @param none
+	 * @return String - the name of the item
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/** Method setWeapon
+	 * sets the item to a weapon
+	 * 
+	 * @param none
+	 * @return void
+	 */
+	public void setWeapon() {
+		weapon = true;
+	}
+
+	/** Method isWeapon
+	 * determines if an item is a weapon
+	 * 
+	 * @param none
+	 * @return boolean - true if it is a weapon, false if not
+	 */
+	public boolean isWeapon() {
+		return weapon;
+	}
+	
+	public int getValue() {
+		
+		return value;
+	}
+	
+	public String toString()
+	{
+		return name + " " + weapon;
+	}
+
+	@Override
+	/** Method equals
+	 * determines if an item is the same as the implicit item
+	 * 
+	 * @param Object - item to be compared
+	 * @return boolean - true if they have the same name, false if different
+	 */
+	public boolean equals(Object obj) {
+		if (name.equals(((Item) obj).getName())) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+}
