eb7a8a475813372bf93db537776db97949ec28a3
Cmorgan5 <Cory_Morgan16@yahoo.com>
2015-04-26 16:35:15 -0400

    Added necessary getters and setters

--- ITEC3860 Game/src/GameTest.java
+++ ITEC3860 Game/src/GameTest.java
@@ -18,57 +18,60 @@ public class GameTest {
 	
 	
 	
-
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
-		
-		Scanner input = new Scanner(System.in);
-		String line = input.nextLine();
-		
-		Item potion = new Item("potion", 3);
-		Item weapon = new Item("weapon", 20);
-		
+//		Scanner input = new Scanner(System.in);
+//		
+//		
+//		Item potion = new Item("potion", 3);
+//		Item weapon = new Item("weapon", 20);
+//		weapon.setWeapon();
+//		
 		Player p = new Player("Cory");
-		Player enemy = new Player("Enemy");
-		p.getGreeting();
-		System.out.println("Player current health:" + p.getHealth());
-		System.out.println("Enemy current health: " + enemy.getHealth());
-		System.out.println("Would you like to attack the enemy?");
-		//System.out.println("With what weapon?");
-		if( input.nextLine().equalsIgnoreCase("yes"))
-				{
-			
-			p.attack(enemy, 20);
-			
-				}
-		
-		else
-			
-			System.out.println("Wrong answer!");
-		
-		System.out.println("Player current health:" + p.getHealth());
-		System.out.println("Enemy current health: " + enemy.getHealth());
-		enemy.toString();
-		//enemy.takeDamage(20);
-		
-		
-		
-		ArrayList<Item> inventory = new ArrayList<Item>();
-		
-		inventory.add(potion);
-		inventory.add(weapon);
-		System.out.println(inventory);
-		//p.isItemAvailable(weapon);
-	    
-//	player.getHealth();
-//	player.toString();
-//	enemy.getHealth();
-//	enemy.toString();
-//	player.attack(enemy);
-//	enemy.toString();
-//	//enemy.takeDamage();
-//	//enemy.g
+//		Enemy enemy = new Enemy("Enemy");
+//		p.getGreeting();
+//		System.out.println("Player current health:" + p.getHealth());
+//		System.out.println("Enemy current health: " + enemy.getHealth());
+//		System.out.println("Would you like to attack the enemy?");
+//		String line = input.nextLine();
+//		
+//		if( line.equalsIgnoreCase("yes"))
+//				{
+//			
+//			p.attack(enemy, weapon.getValue());
+//			
+//				}
+//		
+//		else
+//		{
+//			System.out.println("Wrong answer!");
+//			
+//		}
+//		
+//		
+//		System.out.println("Player remaining health:" + p.getHealth());
+//		System.out.println("Enemy remanining health: " + enemy.getCurrenthealth());
+//		enemy.toString();
+//		//enemy.takeDamage(20);
+//		
+//		
+//		
+//		ArrayList<Item> inventory = new ArrayList<Item>();
+//		
+//		inventory.add(potion);
+//		inventory.add(weapon);
+//		System.out.println(inventory);
+//		//p.isItemAvailable(weapon);
+//	    
+////	player.getHealth();
+////	player.toString();
+////	enemy.getHealth();
+////	enemy.toString();
+////	player.attack(enemy);
+////	enemy.toString();
+////	//enemy.takeDamage();
+////	//enemy.g
 	 
 	 
 
